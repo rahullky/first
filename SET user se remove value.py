e73ce37ab534ca3fb9a0aad3b65a618dda082023
Rahullky <146576412+rahullky@users.noreply.github.com>
2023-10-16 20:31:04 +0530

    Update SET user se remove value.py

--- SET user se remove value.py
+++ SET user se remove value.py
@@ -1,4 +1,5 @@
-# user define sets me remoive value karana
+# SET me  user se input  lena (remove value) ko remove ............*
+
 x={1,5,8,6,"arjun","karan",45,888,"moni"}
 for i in(0,x):
     a=eval(input("enter the remove value "))
